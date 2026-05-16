d232eea03f67769e5ce821e30be729a07f6c5d2c
Abyss <milo04@naver.com>
2026-05-17 00:05:39 +0900

    feat: 모든 페이지 네비게이션 바, footer 연동 및 디자인 충돌 해결

--- js/common.js
+++ js/common.js
@@ -1,1 +1,16 @@
 // 공통 기능. 로그인 상태 확인, 공통 메뉴 처리, 페이지 이동
+
+// 1. 네비게이션 파일(nav.html) 통째로 불러와서 #header에 넣기
+$('#header').load('../component/nav.html');
+
+// 2. .navbar-toggle 누르면 열고 닫기
+$(document).on('click', '.navbar-toggle', function() {
+    var target = $(this).attr('data-target'); // '#myNavbar' 글자 가져오기
+    $(target).collapse('toggle');            // 메뉴창 열고 닫기 끝!
+});
+
+    fetch('../component/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
